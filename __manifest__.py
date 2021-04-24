69b876201d4e71908cc3ae1b43abbfc2a91a92ee
Your Name <you@example.com>
2021-04-23 23:37:53 -0400

    Dependencia de enotif_woo

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','stock','enotif_woo'],
 
     # always loaded
     'data': [
